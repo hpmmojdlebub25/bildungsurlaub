2d8dff3b4462dbc7a67429e411b6662845b15c7e
Benjamin Rieck <hpmmojdlebub25@exece.com>
2025-07-18 10:45:33 +0200

    protype

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+# %% packages
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(usecwd=True))
+
+
+model = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
+
+
+st.title("Unser erster Chatbot")
+user_input = st.text_input("Gib hier deine Frage ein")
+if user_input is not None:
+    messages = [
+        ("system", "Du bist ein Assistent, der hilft, Fragen zu beantworten."),
+        ("user", user_input)
+    ]
+    prompt_template = ChatPromptTemplate.from_messages(messages)
+    chain = prompt_template | model | StrOutputParser()
+    response = chain.invoke({"topic": user_input})
+    st.chat_message("user").write(user_input)
+    st.chat_message("assistant").write(response)
+
+# %%
+
+
+
+
+
